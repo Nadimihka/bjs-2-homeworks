b5a5d4cb4694cde481bb984a77d691508ceb87a0
Nadezhda Krutova <nadiya-yand@yandex.ru>
2025-10-06 20:41:12 +0300

    task done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,102 @@
-function getArrayParams(...arr) {
+"use strict"
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = arr[0];
+  
+  for (let i = 1; i < arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i]
+    }
+    if (arr[i] > max){
+      max = arr[i]
+    }
+    sum += arr[i]
+  }
+  let avg = +(sum / arr.length).toFixed(2);
+
+return { min: min, max: max, avg: avg };
 }
+
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sum = arr[0];
+  for (let i = 1; i < arr.length; i++){
+    sum += arr[i]
+  }
+  return sum;
 }
+
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  // let min = arr[0];
+  // let max = arr[0];
+  
+  // for (let i = 1; i < arr.length; i++){
+  //   if (arr[i] < min){
+  //     min = arr[i]
+  //   }
+  //   if (arr[i] > max){
+  //     max = arr[i]
+  //   }
+  // }
+  // return max-min;
 
+  return Math.max(...arr) - Math.min(...arr);
 }
 
-function differenceEvenOddWorker(...arr) {
 
+function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 === 0){
+      sumEvenElement += arr[i];
+    } else { 
+      sumOddElement += arr[i];
+      }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 === 0){
+      sumEvenElement += arr[i];
+      countEvenElement ++;
+    } 
+  }
+  if (countEvenElement === 0){
+      return 0;
+  } else {
+        return +(sumEvenElement/countEvenElement).toFixed(2);
+    }
 }
 
-function makeWork (arrOfArr, func) {
 
+function makeWork (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++){
+    const result = func(...arrOfArr[i]);
+    console.log("result:",result);
+    if (maxWorkerResult < result){
+      maxWorkerResult = result;
+    }
+  }
+  return maxWorkerResult;
 }
